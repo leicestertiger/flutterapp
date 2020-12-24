40091155fc14f8088ac3c68f9af665be9b3fdcbe
Denise Hill <leicestertiger@yahoo.co.uk>
2020-12-24 12:50:08 +0000

    flutterapp

---
+++ lib/camera_flow.dart
@@ -0,0 +1,79 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterapp/camera_page.dart';
+import 'package:flutterapp/gallery_page.dart';
+import 'package:flutterapp/storage_service.dart';
+
+class CameraFlow extends StatefulWidget {
+  // 1
+  final VoidCallback shouldLogOut;
+
+  CameraFlow({Key key, this.shouldLogOut}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CameraFlowState();
+}
+
+class _CameraFlowState extends State<CameraFlow> {
+  // 2
+  CameraDescription _camera;
+  bool _shouldShowCamera = false;
+  StorageService _storageService;
+
+  // 3
+  List<MaterialPage> get _pages {
+    return [
+      // Show Gallery Page
+      MaterialPage(
+          child: GalleryPage(
+              imageUrlsController: _storageService.imageUrlsController,
+              shouldLogOut: widget.shouldLogOut,
+              shouldShowCamera: () => _toggleCameraOpen(true))),
+
+      // Show Camera Page
+      if (_shouldShowCamera)
+        MaterialPage(
+            child: CameraPage(
+                camera: _camera,
+                didProvideImagePath: (imagePath) {
+                  this._toggleCameraOpen(false);
+                  this._storageService.uploadImageAtPath(imagePath);
+                }))
+    ];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCamera();
+    _storageService = StorageService();
+    _storageService.getImages();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 4
+    return Navigator(
+      pages: _pages,
+      onPopPage: (route, result) => route.didPop(result),
+    );
+  }
+
+  // 5
+  void _toggleCameraOpen(bool isOpen) {
+    setState(() {
+      this._shouldShowCamera = isOpen;
+    });
+  }
+
+  void _getCamera() async {
+    // Ensure that plugin services are initialized so that `availableCameras()`
+// can be called before `runApp()`
+    WidgetsFlutterBinding.ensureInitialized();
+    final camerasList = await availableCameras();
+    setState(() {
+      final firstCamera = camerasList.first;
+      this._camera = firstCamera;
+    });
+  }
+}
---
+++ lib/gallery_page.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterapp/analystics_service.dart';
+import 'package:flutterapp/analytics_events.dart';
+
+// 1
+class GalleryPage extends StatelessWidget {
+  final StreamController<List<String>> imageUrlsController;
+  // 2
+  final VoidCallback shouldLogOut;
+  // 3
+  final VoidCallback shouldShowCamera;
+
+  GalleryPage(
+      {Key key,
+      this.imageUrlsController,
+      this.shouldLogOut,
+      this.shouldShowCamera})
+      : super(key: key) {
+    AnalyticsService.log(ViewGalleryEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+        actions: [
+          // 4
+          // Log Out Button
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child:
+                GestureDetector(child: Icon(Icons.logout), onTap: shouldLogOut),
+          )
+        ],
+      ),
+      // 5
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.camera_alt), onPressed: shouldShowCamera),
+      body: Container(child: _galleryGrid()),
+    );
+  }
+
+  Widget _galleryGrid() {
+    return StreamBuilder(
+        // 1
+        stream: imageUrlsController.stream,
+        builder: (context, snapshot) {
+          // 2
+          if (snapshot.hasData) {
+            // 3
+            if (snapshot.data.length != 0) {
+              return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  // 4
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return CachedNetworkImage(
+                      imageUrl: snapshot.data[index],
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) => Container(
+                          alignment: Alignment.center,
+                          child: CircularProgressIndicator()),
+                    );
+                  });
+            } else {
+              // 5
+              return Center(child: Text('No images to display.'));
+            }
+          } else {
+            // 6
+            return Center(child: CircularProgressIndicator());
+          }
+        });
+  }
+}
